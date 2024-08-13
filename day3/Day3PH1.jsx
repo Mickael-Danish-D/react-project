19bc51358baa0d69598eee5b7e2e587ddb57a565
Mickael-Danish-D <727723eucs119@skcet.ac.in>
2024-08-13 14:40:35 +0530

    Update Day3PH1.jsx

--- day3/Day3PH1.jsx
+++ day3/Day3PH1.jsx
@@ -1,18 +1,36 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const ToggleMessage = () => {
-    const [isMessageVisible, setIsMessageVisible] = useState('true');
+  const [transformation, setTransformation] = useState('Kaioken');
+  const [clickCount, setClickCount] = useState(0);
 
-    const toggleMessage = () => {
-        setIsMessageVisible(!isMessageVisible);
-    };
+  const toggleTransformation = () => {
+    setClickCount((prevClickCount) => {
+      const newClickCount = prevClickCount + 1;
+      if (newClickCount === 4) {
+        setTransformation((prevTransformation) =>
+          prevTransformation === 'Kaioken' ? 'SuperSaiyan' : 'Kaioken'
+        );
+        return 0;
+      }
+      return newClickCount;
+    });
+  };
 
-    return (
-        <div style={{textAlign:'center'}}>
-            <button style={{padding:'10px 20px', marginTop:'200px', fontSize:'bolder'}} onClick={toggleMessage}>{isMessageVisible ? 'Hide Component' : 'Show Component'}</button><br/>
-            {isMessageVisible && <div style={{fontSize:'50px'}}>Hi! How are you!!!</div>}
-        </div>
-    );
+  return (
+    <div style={{
+      display:'flex',
+      flexDirection:'column',
+      justifyContent:'center',
+      alignItems:'center',
+      height:'90vh',
+      textAlign:'center'
+      }}>
+      <h1>Goku's Transformation: {transformation}</h1>
+      <button onClick={toggleTransformation} style={{padding:'10px 20px'}}>Toggle Transformation</button>
+      <p>Click count : {clickCount}</p>
+    </div>
+  );
 };
 
 export default ToggleMessage;
